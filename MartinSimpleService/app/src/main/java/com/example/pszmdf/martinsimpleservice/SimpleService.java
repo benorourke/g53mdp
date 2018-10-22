41d4cf046880bf758502a061c4d9d5a4fc81c4a3
Martin Flintham <mdf@cs.nott.ac.uk>
2018-10-22 10:10:26 +0100

    initial commit

---
+++ MartinSimpleService/app/src/main/java/com/example/pszmdf/martinsimpleservice/SimpleService.java
@@ -0,0 +1,84 @@
+package com.example.pszmdf.martinsimpleservice;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Build;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.util.Log;
+
+public class SimpleService extends Service {
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        // TODO Auto-generated method stub
+        Log.d("g53mdp", "service onBind");
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // TODO Auto-generated method stub
+        Log.d("g53mdp", "service onStartCommand");
+
+        return Service.START_STICKY;
+    }
+
+    private final int NOTIFICATION_ID = 001; // a unique int for each notification
+    private final String CHANNEL_ID = "100";
+
+    @Override
+    public void onCreate() {
+        // TODO Auto-generated method stub
+        Log.d("g53mdp", "service onCreate");
+        super.onCreate();
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "channel name";
+            String description = "channel description";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        Intent intent = new Intent(SimpleService.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        Intent actionIntent = new Intent(SimpleService.this, SimpleService.class);
+        PendingIntent pendingActionIntent = PendingIntent.getService(this, 0, actionIntent, 0);
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("SimpleService")
+                .setContentText("Return to MainActivity?")
+                .setContentIntent(pendingIntent)
+                .addAction(R.drawable.ic_launcher_foreground, "Message Service", pendingActionIntent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        //notificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+        startForeground(NOTIFICATION_ID, mBuilder.build());
+    }
+
+    @Override
+    public void onDestroy() {
+        // TODO Auto-generated method stub
+        Log.d("g53mdp", "service onDestroy");
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.cancel(NOTIFICATION_ID);
+        super.onDestroy();
+    }
+}
